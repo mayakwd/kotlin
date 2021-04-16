e00068ced7aafbaee107a49d49f33028f3a966a9
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2021-04-16 19:35:54 +0200

    KT-46146 `Make '<init>' public` quickfix is not relevant for sealed class inheritors
    ^KT-46146 Fixed

--- idea/src/org/jetbrains/kotlin/idea/quickfix/MakeVisibleFactory.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/MakeVisibleFactory.kt
@@ -17,16 +17,15 @@
 package org.jetbrains.kotlin.idea.quickfix
 
 import com.intellij.codeInsight.intention.IntentionAction
-import org.jetbrains.kotlin.descriptors.ClassConstructorDescriptor
-import org.jetbrains.kotlin.descriptors.ClassDescriptor
-import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
-import org.jetbrains.kotlin.descriptors.DeclarationDescriptorWithVisibility
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.config.LanguageFeature
+import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.descriptors.DescriptorVisibilities.*
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.diagnostics.DiagnosticFactory3
 import org.jetbrains.kotlin.idea.caches.resolve.findModuleDescriptor
+import org.jetbrains.kotlin.idea.project.languageVersionSettings
 import org.jetbrains.kotlin.idea.search.usagesSearch.descriptor
-import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtModifierListOwner
 import org.jetbrains.kotlin.psi.psiUtil.containingClass
@@ -35,6 +34,7 @@ import org.jetbrains.kotlin.resolve.DescriptorUtils
 import org.jetbrains.kotlin.resolve.descriptorUtil.getAllSuperclassesWithoutAny
 
 object MakeVisibleFactory : KotlinIntentionActionsFactory() {
+
     override fun doCreateActions(diagnostic: Diagnostic): List<IntentionAction> {
         val element = diagnostic.psiElement as? KtElement ?: return emptyList()
         val usageModule = element.findModuleDescriptor()
@@ -44,18 +44,28 @@ object MakeVisibleFactory : KotlinIntentionActionsFactory() {
         val descriptor = factory.cast(diagnostic).c as? DeclarationDescriptorWithVisibility ?: return emptyList()
         val declaration = DescriptorToSourceUtils.descriptorToDeclaration(descriptor) as? KtModifierListOwner ?: return emptyList()
 
-        if (declaration.hasModifier(KtTokens.SEALED_KEYWORD) && descriptor is ClassConstructorDescriptor) return emptyList()
+        val isSealedClassConstructor = descriptor is ClassConstructorDescriptor && descriptor.constructedClass.isSealed()
+        if (isSealedClassConstructor && !suggestProtected(declaration)) return emptyList()
 
         val module = DescriptorUtils.getContainingModule(descriptor)
         val targetVisibilities = when (descriptor.visibility) {
-            PRIVATE, INVISIBLE_FAKE -> mutableListOf(PUBLIC).apply {
-                if (module == usageModule) add(INTERNAL)
-                val superClasses = (element.containingClass()?.descriptor as? ClassDescriptor)?.getAllSuperclassesWithoutAny()
-                if (superClasses?.contains(declaration.containingClass()?.descriptor) == true) add(PROTECTED)
+            PRIVATE, INVISIBLE_FAKE -> {
+                if (isSealedClassConstructor) {
+                    listOf(PROTECTED)
+                } else {
+                    mutableListOf(PUBLIC).apply {
+                        if (module == usageModule) add(INTERNAL)
+                        val superClasses = (element.containingClass()?.descriptor as? ClassDescriptor)?.getAllSuperclassesWithoutAny()
+                        if (superClasses?.contains(declaration.containingClass()?.descriptor) == true) add(PROTECTED)
+                    }
+                }
             }
             else -> listOf(PUBLIC)
         }
 
         return targetVisibilities.mapNotNull { ChangeVisibilityFix.create(declaration, descriptor, it) }
     }
+
+    private fun suggestProtected(declaration: PsiElement): Boolean = declaration.languageVersionSettings
+        .supportsFeature(LanguageFeature.AllowSealedInheritorsInDifferentFilesOfSamePackage)
 }
